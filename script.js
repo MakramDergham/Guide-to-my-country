cbcb0d5cc4b60a70318c307534ac80d59685ef95
Makram Dergham <derghammakram@gmail.com>
2024-03-03 18:30:52 +0200

    Italy

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
-document.getElementById('button1').addEventListener('click', function() {
+const button = document.getElementById('button1')
+button.addEventListener('click', function() {
     window.scrollTo({
       top: 800,
       behavior: 'smooth'
@@ -25,30 +25,4 @@ window.addEventListener('load', fullyLoaded)
 function fullyLoaded(){
   preloader.style.display = "none"
 }
-var slideIndex = 1;
-showSlides(slideIndex);
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if(n > slides.length) {
-    slideIndex = 1
-  }
-  if(n < 1) {
-    slideIndex = slides.length
-  }
-  for(i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for(i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
+
